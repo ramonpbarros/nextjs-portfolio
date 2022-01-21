4cbbc2520a86dfe8d77c0fd07951c93bdd409dbf
Sorrab <ramonpbarros@gmail.com>
2022-01-20 21:46:07 -0800

    title index center

--- components/Title/Title.component.js
+++ components/Title/Title.component.js
@@ -4,7 +4,7 @@ import styles from './Title.module.scss';
 export default function Title() {
   return (
     <div className={styles.div}>
-      <h2 className={clsx(styles.h2, styles.rotate, styles.span)}>RAMON</h2>
+      <h2 className={clsx(styles.h2, styles.rotate, styles.ramon)}>RAMON</h2>
       <h2 className={clsx(styles.h2, styles.barros)}>BARROS</h2>
     </div>
   );
