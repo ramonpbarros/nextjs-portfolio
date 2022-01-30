ae92d4f781a6ae49da7f2c50fc136669ff21cf3c
Sorrab <ramonpbarros@gmail.com>
2022-01-29 22:24:12 -0800

    added test p tag in about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -38,6 +38,7 @@ function About() {
           With each project, my goal is to best engage my audience for an
           impactful user-experience.
         </p>
+        <p>test</p>
         <p>Interested on getting to know me better? <Link href='/contact' className='test'>Let&#39;s talk</Link> </p>
       </section>
     </>
