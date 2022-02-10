9a3d4ba1b73f5471719047774a073443383ae541
Sorrab <ramonpbarros@gmail.com>
2022-02-09 17:05:03 -0800

    test

--- pages/about/index.js
+++ pages/about/index.js
@@ -38,6 +38,19 @@ function About() {
           With each project, my goal is to best engage my audience for an
           impactful user-experience.
         </p>
+        <p className='mt-4'>
+          Born and raised in Brazil &#127463;&#127479;<br/>I&#39;ve been living in San Diego, California for the past 15 years and during
+          this time I had some experience working with mobile development, but
+          mostly working on my own business. I was in the restaurant business
+          for 5 years, always applying technical tools in order to increase
+          sales. I earned a certificate in Full Stack Web Development from the
+          University of California San Diego, with newly developed skills. With
+          a focus on front-end and back-end web development, I was able to test
+          my skills building different projects either by myself or in groups.
+          With each project, my goal is to best engage my audience for an
+          impactful user-experience.
+        </p>
+        <p>test3</p>
         <p>test3</p>
         <p>Interested on getting to know me better? <Link href='/contact' className='test'>Let&#39;s talk</Link> </p>
       </section>
